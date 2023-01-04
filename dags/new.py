6c7a9660e9814e513e0bdfb6e1830b27f8f7bb41
VitorRussomanoCGCOMPASS <Vitor.Ibanez@cgcompass.com>
2023-01-04 12:55:31 -0300

    new

--- dags/new.py
+++ dags/new.py
@@ -1,50 +1,20 @@
 import datetime
 
 # from pendulum import datetime as p_datetime
-import logging
-
-import yaml
-from dateutil import parser
 
 from airflow import DAG
 from airflow.models.baseoperator import chain
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import ShortCircuitOperator
 
-# TODO : THE HOLIDAY FILE SHOULD NOT BE IN THE DAGS DIRECTORY.
-# TODO : THE IS_NOT_HOLIDAY WILL BE USED SEVERAL TIMES, IT SHOULD BE SOMEWHERE ELSE TO BE IMPORTED.
+from operators.hol import _is_not_holiday
 
 # COMPLETE :  CHECK FOR DS
 # COMPLETE : THE DATE FORMAT IS IMPORTANT IN THIS SCENARIO.
 
 
-def _is_not_holiday(ds) -> bool:
-    """
-    Check if execution date (ds) is a holiday or not
-
-    Parameters
-    ----------
-    ds : str
-        Execution date provided by airflow
-    
-    # FIXME : yesterday_ds_nodash -> Indicates yesterday. 
-    
-    Returns
-    -------
-    bool
-        True
-
-    """
-    with open("dags/holidays.yml", "r") as f:
-
-        doc = yaml.load(f, Loader=yaml.SafeLoader)
-        ds = datetime.datetime.strptime(ds, "%Y-%m-%d")
-        doc_as_datetime = [parser.parse(date, dayfirst=False) for date in doc["Data"]]
-        logging.info(ds)
-        logging.info(doc_as_datetime[-1])
-        if ds in doc_as_datetime:
-            return False
-        return True
+# COMPLETE : WILL TEST IMPORTING THE IS_NOT_HOLIDAY
+# TODO : WILL LATER ON TRY TO MOVE IT TO THE INCLUDE DIRECTORY
 
 
 with DAG(
