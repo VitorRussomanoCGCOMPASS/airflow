98c5a5f3a04e21651b793a1127918f3974ffa34e
VitorRussomanoCGCOMPASS <Vitor.Ibanez@cgcompass.com>
2023-01-09 13:12:27 -0300

    template generated anbima

--- dags/resultados_ima.py
+++ dags/resultados_ima.py
@@ -4,20 +4,14 @@ from sensors.anbima import AnbimaSensor
 from utils.hol import _is_not_holiday
 
 from airflow import DAG
-from airflow.macros import ds_add
-from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 
-# TODO  :  TEMPLATE GENERATE DAG
- 
-with DAG(
-    dag_id="anbima_dynamic",
-    description=None,
-    start_date=datetime(2023, 1, 1),
-    schedule="@daily",
-    catchup=False,
-):
+default_args = {"owner": "airflow", "start_date": datetime(2023, 1, 1)}
 
+# TODO : MOVE IT FROM PLUGINS.
+
+
+with DAG('resultados_ima', schedule="@daily", default_args=default_args, catchup=False):
     holiday = PythonOperator(
         task_id="is_not_holiday", python_callable=_is_not_holiday, provide_context=True
     )
@@ -25,7 +19,7 @@ with DAG(
     wait = AnbimaSensor(
         task_id="wait_for_data",
         headers={"data": "{{ macros.ds_add(ds, -1) }}"},
-        endpoint="/feed/precos-indices/v1/indices-mais/resultados-ima",
+        endpoint='/feed/precos-indices/v1/indices-mais/resultados-ima',
         mode="reschedule",
         timeout=60 * 60,
         data=None,
@@ -35,15 +29,12 @@ with DAG(
 
     fetch = AnbimaOperator(
         task_id="fetch_data",
-        endpoint="/feed/precos-indices/v1/indices-mais/resultados-ima",
+        endpoint='/feed/precos-indices/v1/indices-mais/resultados-ima',
         headers={"data": "{{ macros.ds_add(ds, -1) }}"},
         output_path="C:/Users/Vitor Russomano/airflow/data/custom_operator/'{{macros.ds_add(ds,-1)}}'.json",
     )
 
-    pull = EmptyOperator(task_id="pull")
 
     holiday.set_downstream(wait)
     wait.set_downstream(fetch)
-    fetch.set_downstream(pull)
-
-# https://api.anbima.com.br:443/feed/precos-indices/v1/indices-mais/resultados-ima?grant_type=client_credentials
+    
--- plugins/connections/britech.py
+++ plugins/connections/britech.py
@@ -1,5 +1,15 @@
 from airflow.models.connection import Connection
 
+# TODO : MOVE IT TO THE SYSTEM.
+
 BRITECH_CONNECTION = Connection(
-    
+    conn_id = 'britech_api',
+    conn_type='http',
+    description=None,
+    login='webservice',
+    password='Compass@1',
+    host='saas.britech.com.br/compass_ws/api/',
+    port=443,
+    schema='https'
+
 )
