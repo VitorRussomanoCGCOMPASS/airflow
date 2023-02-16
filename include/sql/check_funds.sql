9cee4c940b4f75241db1daaee0065bdbf688f130
VitorRussomanoCGCOMPASS <Vitor.Ibanez@cgcompass.com>
2023-02-16 16:58:48 -0300

    check_funds d-1

--- include/sql/check_funds.sql
+++ include/sql/check_funds.sql
@@ -1,6 +1,6 @@
 -- Check if all funds have value in d-1
 SELECT ( SELECT COUNT(*)
           FROM funds_values
-         WHERE funds_id IN ( SELECT britech_id FROM funds) 
+         WHERE funds_id IN ( SELECT britech_id FROM funds WHERE status='ativo') 
 	   AND date ='{{macros.anbima_plugin.forward(ds,-1)}}') =
-	   		(SELECT COUNT(*) FROM funds)
+	   		(SELECT COUNT(*) FROM funds WHERE status='ativo')
