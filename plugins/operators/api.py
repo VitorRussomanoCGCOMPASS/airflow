1230edeec770533ed44894826b4c7e79f6501175
VitorRussomanoCGCOMPASS <Vitor.Ibanez@cgcompass.com>
2023-03-24 11:11:00 -0300

    remove base api, rename module and reconstruct Anbima and Britech operator

---
+++ plugins/operators/api.py
@@ -0,0 +1,150 @@
+from typing import Callable
+
+from hooks.anbima import AnbimaHook
+from hooks.britech import BritechHook
+
+from airflow.exceptions import AirflowException
+from airflow.models.baseoperator import BaseOperator
+from airflow.utils.context import Context
+from airflow.utils.operator_helpers import determine_kwargs
+
+
+class BritechOperator(BaseOperator):
+    """
+    Get data from an Britech API endpoint in JSON format.
+
+    :param data: The data to pass (templated)
+    :param headers: The HTTP headers to be added to the GET request
+    :param response_check: A check against the 'requests' response object.
+        The callable takes the response object as the first positional argument
+        and optionally any number of keyword arguments available in the context dictionary.
+        It should return True for 'pass' and False otherwise.    :param extra_options:
+    :param log_response: Log the response (default: False)
+    :param request_params: params in the URL for a GET request (templated)
+    :param endpoint: The relative part of the full url.
+    :param json: payload to push
+    """
+
+    template_fields = (
+        "data",
+        "headers",
+        "request_params",
+    )
+
+    def __init__(
+        self,
+        *,
+        britech_conn_id="britech-api",
+        data: str | dict | None = None,
+        headers: dict | None = None,
+        response_check: Callable[..., bool] | None = None,
+        extra_options: dict | None = None,
+        log_response: bool = False,
+        request_params: dict | None = None,
+        endpoint: str,
+        json: dict | None = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.britech_conn_id = britech_conn_id
+        self.endpoint = endpoint
+        self.headers = headers or {}
+        self.request_params = request_params or {}
+        self.extra_options = extra_options or {}
+        self.response_check = response_check
+        self.log_response = log_response
+        self.data = data
+        self.json = json
+
+    def execute(self, context: Context):
+        "Call an specific API endpoint and generate the response"
+
+        hook = BritechHook(method="GET", conn_id=self.britech_conn_id)
+
+        self.log.info("Calling HTTP Method")
+        response = hook.run(
+            endpoint=self.endpoint,
+            data=self.data,
+            headers=self.headers,
+            request_params=self.request_params,
+            extra_options=self.extra_options,
+            json=self.json,
+        )
+
+        if self.log_response:
+            self.log.info(response.text)
+
+        if self.response_check:
+            kwargs = determine_kwargs(self.response_check, [response], context)
+            if not self.response_check(response, **kwargs):
+                raise AirflowException("Response check returned False.")
+
+        return response.json()
+
+
+class AnbimaOperator(BaseOperator):
+    """
+    Get data from the Anbima API endpoint in JSON format.
+
+    :param data: The data to pass (templated)
+    :param headers: The HTTP headers to be added to the GET request
+    :param response_check: A check against the 'requests' response object.
+        The callable takes the response object as the first positional argument
+        and optionally any number of keyword arguments available in the context dictionary.
+        It should return True for 'pass' and False otherwise.    :param extra_options:
+    :param log_response: Log the response (default: False)
+    :param request_params: params in the URL for a GET request (templated)
+    :param endpoint: The relative part of the full url.
+    """
+    template_fields = (
+        "data",
+        "headers",
+        "request_params",
+    )
+
+    def __init__(
+        self,
+        *,
+        anbima_conn_id="anbima-api",
+        data: str | dict | None = None,
+        headers: dict | None = None,
+        response_check: Callable[..., bool] | None = None,
+        extra_options: dict | None = None,
+        log_response: bool = False,
+        request_params: dict | None = None,
+        endpoint: str,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.anbima_conn_id = anbima_conn_id
+        self.endpoint = endpoint
+        self.headers = headers or {}
+        self.request_params = request_params or {}
+        self.extra_options = extra_options or {}
+        self.response_check = response_check
+        self.log_response = log_response
+        self.data = data
+
+    def execute(self, context: Context):
+        "Call an specific API endpoint and generate the response"
+
+        hook = AnbimaHook(conn_id=self.anbima_conn_id)
+
+        self.log.info("Calling HTTP Method")
+        response = hook.run(
+            endpoint=self.endpoint,
+            data=self.data,
+            headers=self.headers,
+            request_params=self.request_params,
+            extra_options=self.extra_options,
+        )
+
+        if self.log_response:
+            self.log.info(response.text)
+
+        if self.response_check:
+            kwargs = determine_kwargs(self.response_check, [response], context)
+            if not self.response_check(response, **kwargs):
+                raise AirflowException("Response check returned False.")
+
+        return response.json()
