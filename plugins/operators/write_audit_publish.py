8756ccada2d434ac035b0675d3443a864d1bd67c
VitorRussomanoCGCOMPASS <Vitor.Ibanez@cgcompass.com>
2023-03-31 10:12:34 -0300

    smll changes

--- plugins/operators/write_audit_publish.py
+++ plugins/operators/write_audit_publish.py
@@ -1,114 +1,11 @@
-from contextlib import closing
-from typing import Any, Iterable, Literal, Sequence
+from typing import Any, Iterable, Sequence
 
-from pyodbc import ProgrammingError
 from sqlalchemy import Column, MetaData, Table
 from sqlalchemy.orm.decl_api import DeclarativeMeta
 
 from airflow.models.xcom_arg import XComArg
 from airflow.providers.common.sql.operators.sql import BaseSQLOperator
 from airflow.utils.context import Context
-
-
-class TemporaryTableSQLOperator(BaseSQLOperator):
-
-    template_fields: Sequence[str] = ()
-    template_ext: Sequence[str] = ()
-
-    def __init__(
-        self,
-        *,
-        table: str | DeclarativeMeta,
-        target_fields: Iterable[str] | None = None,
-        temporary_table: str | None = None,
-        empty: bool = True,
-        conn_id: str | None = None,
-        database: str | None = None,
-        **kwargs,
-    ) -> None:
-
-        if database is not None:
-            hook_params = kwargs.pop("hook_params", {})
-            kwargs["hook_params"] = {"database": database, **hook_params}
-
-        super().__init__(
-            conn_id=conn_id,
-            **kwargs,
-        )
-
-        self.table = table
-        self.target_fields = target_fields or ()
-        self.temporary_table = temporary_table
-        self.empty = empty
-
-    @classmethod
-    def _generate_statement_sql(
-        cls,
-        table: str | DeclarativeMeta,
-        target_fields: Iterable[str],
-        temporary_table: str | None,
-        empty: bool,
-        **kwargs,
-    ) -> str:
-
-        if target_fields:
-            if not isinstance(target_fields, Iterable):
-                raise Exception(
-                    "Trailing commas required for a unique element : e.g. (EmployeeId,)"
-                )
-            target_fields = ", ".join(target_fields)
-        else:
-            target_fields = "*"
-
-        if isinstance(table, DeclarativeMeta):
-            table = table.__name__
-
-        if not temporary_table:
-            temporary_table = table + "_temp"
-
-        sql = f"SELECT {target_fields} INTO {temporary_table} FROM {table}"
-
-        if not empty:
-            end = ";"
-        else:
-            end = " WHERE 0=1;"
-
-        sql += end
-        return sql
-
-    def execute(self, context: Context):
-
-        extra_kwargs = {}
-
-        hook = self.get_db_hook()
-
-        with closing(hook.get_conn()) as conn:
-
-            cur = conn.cursor()
-
-            sql = self._generate_statement_sql(
-                self.table,
-                self.target_fields,
-                self.temporary_table,
-                self.empty,
-                **extra_kwargs,
-            )
-
-            self.log.debug("Generated sql: %s", sql)
-            # WHY DO WE OPT FOR CUR.EXECUTE INSTEAD OF HOOK RUN?
-            try:
-                cur.execute(sql)
-            except ProgrammingError as ex:
-                if ex.args[0] == "42S01":
-                    self.log.error("Table already exists")
-                    raise
-
-            finally:
-                cur.close()
-
-            conn.commit()
-
-        self.log.info("Done. Created temporary table.")
 
 
 class InsertSQLOperator(BaseSQLOperator):
@@ -155,13 +52,6 @@ class InsertSQLOperator(BaseSQLOperator):
             conn.execute(self.table.insert().values(self.values))
 
         self.log.info("Sucessfully inserted values into table :%s", self.table.name)
-
-
-# TODO : IF ON CONFLICT DO UPDATE
-""" 
-WHEN MATCHED THEN
-UPDATE SET ....
-"""
 
 
 class MergeSQLOperator(BaseSQLOperator):
@@ -259,18 +149,19 @@ class MergeSQLOperator(BaseSQLOperator):
                 WHEN MATCHED THEN
                 UPDATE SET 
                 """
-            
+
         for col in columns:
             if isinstance(col, str):
                 # We make sure that there is a column with that name for safety
                 try:
-                    col =   getattr((getattr(source_table, "columns")), col)
-                except AttributeError as exc:
-                    raise exc
-            
+                    col = getattr((getattr(source_table, "columns")), col)
+                except AttributeError:
+                    raise Exception(
+                        "There is no column named %s in table %s", col, source_table
+                    )
+
             if isinstance(col, Column):
                 stmt += f"target.{col.name} = source.{col.name}"
-                
 
         return stmt
 
@@ -328,9 +219,7 @@ class MergeSQLOperator(BaseSQLOperator):
             )
             sql += update_stmt
 
-        sql +=';'
-
-        return sql
+        return sql + ";"
 
     def execute(self, context: Context):
 
@@ -349,11 +238,9 @@ class MergeSQLOperator(BaseSQLOperator):
             holdlock=self.holdlock,
             set_=self.set_,
         )
-        
+
         self.log.debug("Generated sql: %s", sql)
-       
+
         hook.run(sql)
-       
+
         self.log.info("Sucessfuly merged.")
-
-
